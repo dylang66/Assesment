c7d68dbbfc8f0ce75302886ef6e75155c54fb918
Dylan <dylangaldes.1999@hotmail.com>
2024-08-07 19:54:20 +0200

    Web Api to get all Appointments from DB

---
+++ AssesmentAPI/Controllers/AppointmentsController.cs
@@ -0,0 +1,26 @@
+using Assesment.Models;
+using Assesment.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AssesmentAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly AppointmentRepository _AppointmentRepository;
+        private readonly ILogger<AppointmentsController> _logger;
+        public AppointmentsController(AppointmentRepository appRepo, ILogger<AppointmentsController> logger) {
+            _AppointmentRepository = appRepo;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Appointment> Get()
+        {
+            return _AppointmentRepository.GetAll().ToList();
+        }
+    }
+}
---
+++ AssesmentAPI/Program.cs
@@ -0,0 +1,24 @@
+using Assesment.Data;
+using Assesment.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<AppointmentRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
